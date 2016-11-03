6c693d44edc6b39df472bd1216a3b3ecde52cc72
TakayukiKiyohara <nezumimusume30@gmail.com>
2016-11-03 23:47:08 +0900

    Tutorial02を追加。

---
+++ Tutorial02/Assets/AnimationTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Animator animator = GetComponent<Animator>();
+        if (Input.GetKey(KeyCode.UpArrow)
+            || Input.GetKey(KeyCode.DownArrow)
+            || Input.GetKey(KeyCode.LeftArrow)
+            || Input.GetKey(KeyCode.RightArrow)
+            )
+        {
+            animator.SetBool("Run", true);
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetTrigger("Attack");
+        }
+       
+	}
+}
---
+++ Tutorial02/Assets/Character.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+    Vector3 moveSpeed = new Vector3();
+    Vector3 direction = new Vector3();
+	// Use this for initialization
+	void Start () {
+        direction = Vector3.forward;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CharacterController charaCtr = GetComponent<CharacterController>();
+        moveSpeed.x = 0.0f;
+        moveSpeed.z = 0.0f;
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            moveSpeed.x = 0.1f;   
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            moveSpeed.x = -0.1f;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            moveSpeed.z = 0.1f;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            moveSpeed.z = -0.1f;
+        }
+        if(Input.GetKey(KeyCode.J) == true)
+        {
+            moveSpeed.y = 0.5f;
+        }
+
+        moveSpeed.y -= 0.05f;
+        Vector3 dirTmp = moveSpeed;
+        dirTmp.y = 0.0f;
+        if (dirTmp.magnitude > 0.0f)
+        {
+            direction = dirTmp.normalized;
+        }
+        charaCtr.Move(moveSpeed);
+        if (charaCtr.isGrounded)
+        {
+            //地面に着いている。
+            moveSpeed.y = 0.0f;
+        }
+        
+        transform.localRotation = Quaternion.LookRotation(direction);
+    }
+}
