2fbf644e913d2400b1ef983aa88ce61e2a760620
KawaharaKiyohara <nezumimusume@gmail.com>
2016-11-12 18:03:30 +0900

    GameManagerをコミット。

---
+++ Shooting/Assets/script/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// ゲームマネージャークラス。
+/// </summary>
+public class GameManager : MonoBehaviour {
+    public bool isGameOver;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
