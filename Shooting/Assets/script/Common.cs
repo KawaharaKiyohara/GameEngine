e0f5a50368b40230e99ae1ceb0b536bbd6c132e3
TakayukiKiyohara <t>
2016-11-18 12:06:16 +0900

    fadeOutの処理を実装。

---
+++ Shooting/Assets/script/Common.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Common : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnLevelWasLoaded(int level)
+    {
+        //フェードアウトコンポーネントを張り付ける。
+        FadeOut fadeOut = gameObject.GetComponent<FadeOut>();
+        if (fadeOut == null)
+        {https://docs.unity3d.com/ja/current/ScriptReference/Microphone.html
+            gameObject.AddComponent<FadeOut>();
+        }
+    }
+
+}
