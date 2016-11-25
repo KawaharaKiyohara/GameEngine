3e2d6125c3927affe4bfa9d311f934fd31f9d8db
inoue <a>
2016-11-25 09:50:07 +0900

    t

--- Shooting/Assets/script/Title.cs
+++ Shooting/Assets/script/Title.cs
@@ -10,22 +10,23 @@ public class Title : MonoBehaviour {
     };
     public GameObject goCommon;
     public GameObject goCommonCanvas;
+    public GameObject Titlemusic;
     Step step = Step.WaitPressAnyKey;
 
 	// Use this for initialization
 	void Start () {
         goCommon = GameObject.Find("Common");
         DontDestroyOnLoad(goCommon);
+        Instantiate(Titlemusic);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
         switch (step) {
             case Step.WaitPressAnyKey:
                 if (Input.GetButton("Fire1"))
                 {
                     goCommon.AddComponent<FadeIn>();
-
                     step = Step.WaitFadeOut;
                 }
                 break;
