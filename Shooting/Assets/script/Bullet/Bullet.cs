0a89feb2205e8d5d9e24dae3423557d5c416e365
TakayukiKiyohara <1>
2017-01-19 09:45:55 +0900

    弾丸が画面外に行くと正しく消えるようにした。

--- Shooting/Assets/script/Bullet/Bullet.cs
+++ Shooting/Assets/script/Bullet/Bullet.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class Bullet : MonoBehaviour {
     public Vector3 moveDir;
     public GameObject EnemyExplosion;
+    Camera mainCamera;
     // Use this for initialization
     void Start () {
-	
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>(); ;
 	}
 	
 	// Update is called once per frame
@@ -14,12 +15,18 @@ public class Bullet : MonoBehaviour {
         Vector3 pos = transform.localPosition;
         pos += moveDir * 0.05f;
         transform.localPosition = pos;
-	}
-    void OnBecameInvisible()
-    {
-        //画面外に行ったら削除。
-        Object.Destroy(gameObject);
+        pos = mainCamera.WorldToScreenPoint(pos);
+        if((int)pos.x < mainCamera.pixelRect.xMin
+            || (int)pos.x > mainCamera.pixelRect.xMax
+            || (int)pos.y < mainCamera.pixelRect.yMin
+            || (int)pos.y > mainCamera.pixelRect.yMax
+        )
+        {
+            //画面外。
+            Destroy(gameObject);
+        }
     }
+   
     /// <summary>
     /// この弾丸を発射したエネミーが死んだことを通知。
     /// </summary>
